f1e1d5ad2415ae5d99b41b52786804ef1a841687
Akash <chakash793@gmail.com>
2025-07-29 15:58:35 +0530

    Add variable declarations and type printing in main function

--- metal-variables/main.go
+++ metal-variables/main.go
@@ -6,4 +6,12 @@ func main() {
 	var username string = "Devleo"
 	fmt.Println("Hello, " + username + "!")
 	fmt.Printf("Variable is of type %T\n", username)
+
+	var isLoggedIn bool = true
+	fmt.Println(isLoggedIn)
+	fmt.Printf("Variable is of type:%T\n", isLoggedIn)
+
+	var anotherVariable string
+	fmt.Println(anotherVariable)
+	fmt.Printf("Variable is of type:%T\n", anotherVariable)
 }
